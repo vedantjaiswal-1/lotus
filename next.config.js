db76648a16447f36e8689bb617e3315b3b37ae45
vedant jaiswal <vedantjaiswal1@gmail.com>
2022-06-17 13:57:27 +0530

    format(truetype) to format(ttf)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true
 };
-const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = nextConfig;
 
 module.exports = {
@@ -12,7 +12,3 @@ module.exports = {
   }
 };
 
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/6.7.96/fonts/materialdesignicons-webfont.woff2' : '',
-}
